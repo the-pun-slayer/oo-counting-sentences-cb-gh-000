29af5dfcc0a971a08816d1cce89493a84d7f5ffe
PUNEET MANDIL <puneetmandil.mec18@itbhu.ac.in>
2020-06-08 09:07:16 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count= self.split(/\.|\?|\!/).delete_if do |w|    
+                          w.size < 2
+            end
+            
+            count.size
   end
 end
